ef6c7b2c74ddf2cb50546cbd3d577ea2c019d6d2
Maxime Moreau <contact@maxime-moreau.fr>
2019-11-25 19:22:21 +0100

    add(raspberry:mqtt): connect to the broker

---
+++ raspberrypi_central/mqtt/index.js
@@ -0,0 +1,52 @@
+const mqtt = require('mqtt')
+const fs = require('fs')
+
+require('dotenv').config()
+
+const getMqttClient = () => {
+  const options = {
+    username: process.env.MQTT_USERNAME,
+    password: process.env.MQTT_PASSWORD,
+    host: process.env.MQTT_HOST,
+    port: 8883,
+    protocol: 'mqtts',
+  
+    // @TODO see https://stackoverflow.com/questions/40018804/node-js-mqtt-client-using-tls
+    rejectUnauthorized: false,
+    // key: fs.readFileSync('client-key.pem'),
+    // cert: fs.readFileSync('client-cert.pem'),
+    // ca: [ fs.readFileSync('../docker/mosquitto/config/certs/rootCA.crt') ],
+  }
+  
+  return mqtt.connect(options)
+}
+
+const client = getMqttClient()
+
+client.on('connect', () => {
+  client.subscribe('presence', err => {
+    if (!err) {
+      client.publish('presence', 'Hello mqtt')
+    }
+  })
+})
+
+const presenceHandler = (message) => {
+  // message is Buffer
+  console.log(message.toString(), 'from handler!')
+}
+
+const routes = {
+  presence: presenceHandler
+}
+
+const routesNames = Object.keys(routes)
+
+client.on('message', function (topic, message) {
+
+  if (routesNames.includes(topic)) {
+    routes[topic](message)
+  }
+
+  client.end()
+})

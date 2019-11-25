0d43652f7c0303eb4c74c61226ebaa6930e385be
Maxime Moreau <contact@maxime-moreau.fr>
2019-11-25 23:21:59 +0100

    add(esp:mqtt): secure mqtt conn done :tada:
    But I have to clean the code, It's a little bit messy.

---
+++ esp8266-1/SetupWifi.h
@@ -0,0 +1,76 @@
+#ifndef _SetupWifi_H_
+#define _SetupWifi_H_
+
+
+#include <ESP8266WiFi.h>
+#include "AsyncWait.h"
+
+
+class SetupWifi {
+public:
+    SetupWifi(
+        const char *ssid_,
+        const char *password_,
+        const char *ca_cert_PROG_,
+        const char *client_cert_PROG_,
+        const char *client_key_PROG_
+    )
+     : ssid(ssid_)
+     , password(password_)
+
+     , ca_cert_PROG(ca_cert_PROG_)
+     , client_cert_PROG(client_cert_PROG_)
+     , client_key_PROG(client_key_PROG_)
+
+     , x509CaCert(ca_cert_PROG)
+     , x509ClientCert(client_cert_PROG)
+     , PrivateClientKey(client_key_PROG)
+    {
+        wifiClient.setTrustAnchors(&x509CaCert);
+        wifiClient.setClientRSACert(&x509ClientCert, &PrivateClientKey);
+    }
+
+    virtual ~SetupWifi() { }
+
+    WiFiClient & getWiFiClient() {
+        return wifiClient;
+    }
+
+    BearSSL::WiFiClientSecure & getWifiSecureClient() {
+        return wifiClient;
+    }
+
+    bool isReadyForProcessing() {
+        return (setClock_status > FINISHED);
+    }
+
+    String getMacAddress();
+    void setupWifi();
+    void loopWifi();
+
+private:
+    const char *ssid;
+    const char *password;
+
+    const char *ca_cert_PROG;
+    const char *client_cert_PROG;
+    const char *client_key_PROG; // KEEP THIS VALUE PRIVATE AND SECURE!!!
+
+    BearSSL::WiFiClientSecure wifiClient;
+    BearSSL::X509List x509CaCert;
+    BearSSL::X509List x509ClientCert;
+    BearSSL::PrivateKey PrivateClientKey;
+
+    // Note: variables of type 'status' are NEVER assigned a value of 'FINISHED'.
+    // 'FINISHED' is only used is comparisons (e.g. if(x_status>FINISHED)... ),
+    // rather than always testing for SUCCESS || FAILED.
+    enum status { NOT_STARTED, STARTED, FINISHED, SUCCESS, FAILED };
+    status setClock_status = NOT_STARTED;
+    AsyncWait setClock_AsyncWait;
+
+    void setClock();
+    void checkClockStatus();
+};
+
+
+#endif // _SetupWifi_H_

5f7dca24b93b539e030cb9c626dd2d025bb46496
Maxime <contact@maxime-moreau.fr>
2020-01-08 15:44:43 +0100

    add(auth): things to auth

--- raspberrypi_central/webapp/app/devices/urls.py
+++ raspberrypi_central/webapp/app/devices/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- raspberrypi_central/webapp/app/devices/views.py
+++ raspberrypi_central/webapp/app/devices/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import LocationsSerializer
 from .models import Locations
 
 class LocationsViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    
     queryset = Locations.objects.all()
     serializer_class = LocationsSerializer

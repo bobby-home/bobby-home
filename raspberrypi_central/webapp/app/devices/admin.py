73ac13b891b1f2dba968137567e34f40b7f80355
Maxime <contact@maxime-moreau.fr>
2020-01-08 21:33:39 +0100

    add(admin models)

--- raspberrypi_central/webapp/app/devices/admin.py
+++ raspberrypi_central/webapp/app/devices/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Location
+from .models import Location, DeviceType, Device, SensorInformation, Sensor
 
 admin.site.register(Location)
+admin.site.register(DeviceType)
+admin.site.register(Device)
+admin.site.register(SensorInformation)
+admin.site.register(Sensor)

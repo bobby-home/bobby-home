2ba031363eb2b1b85244b29c72d4d26fab2bbe5a
Maxime Moreau <contact@maxime-moreau.fr>
2020-01-21 22:04:53 +0100

    add(admin: devices): add device_id field as readonly.
    This is useful to know the device_id field, copy/past it to prepare your device to make request to the system and go!

--- raspberrypi_central/webapp/app/devices/admin.py
+++ raspberrypi_central/webapp/app/devices/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import Location, DeviceType, Device, SensorInformation, Sensor
 
+class DeviceAdmin(admin.ModelAdmin):
+    readonly_fields = ('device_id',)
+
 admin.site.register(Location)
 admin.site.register(DeviceType)
-admin.site.register(Device)
+admin.site.register(Device, DeviceAdmin)
 admin.site.register(SensorInformation)
 admin.site.register(Sensor)

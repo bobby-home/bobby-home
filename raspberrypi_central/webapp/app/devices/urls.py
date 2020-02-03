4f8f032c934786f356561c36c72d015dc3752721
Maxime Moreau <contact@maxime-moreau.fr>
2020-02-03 10:00:54 +0100

    improve: moving debug toolbar from app to global urls

--- raspberrypi_central/webapp/app/devices/urls.py
+++ raspberrypi_central/webapp/app/devices/urls.py
@@ -2,28 +2,12 @@ from django.urls import include, path
 from django.conf.urls import url
 from rest_framework import routers
 from . import views
-from django.conf import settings
 
 router = routers.SimpleRouter()
 router.register(r'location', views.LocationsViewSet)
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
-    ] + urlpatterns
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- raspberrypi_central/webapp/app/hello_django/urls.py
+++ raspberrypi_central/webapp/app/hello_django/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
@@ -22,3 +24,15 @@ urlpatterns = [
     path('alarm/', include('alarm.urls')),
     path('alert/', include('alerts.urls')),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

93398d56b99a596bc92b003ca711ef2687d46b29
Maxime Moreau <contact@maxime-moreau.fr>
2020-02-03 23:27:48 +0100

    feat: send_sms http request with coroutine

--- raspberrypi_central/attachments/app/send_sms.py
+++ raspberrypi_central/attachments/app/send_sms.py
@@ -1,6 +1,10 @@
 from urllib import request, parse
+import asyncio
 import os
 from functools import partial
+import aiohttp
+from aiohttp import ClientSession
+from typing import IO
 
 
 """FREE MOBILE SMS NOTIFICATION
@@ -13,7 +17,7 @@ USER = os.environ['FREE_USER']
 PASSWORD = os.environ['FREE_PASSWORD']
 
 
-def send_sms(user, password, message):
+async def send_sms(user: str, password: str, message: str):
     data = {'user': user, 'pass': password, 'msg': message}
     query = parse.urlencode(data)
     url = 'https://smsapi.free-mobile.fr/sendmsg?{}'.format(query)
@@ -22,20 +26,21 @@ def send_sms(user, password, message):
                   402: 'Spammer!',
                   403: 'Access Denied',
                   500: 'Server Down'}
-   
-    try:
-        request.urlopen(url)
-        return True, 'Success'
-    
-    except request.HTTPError as e:
-        raise e
-        # return False, errorcodes[e.code]
 
-send_sms = partial(send_sms, USER, PASSWORD)
+    async with ClientSession() as session:
+        async with session.get(url) as resp:
+            resp.raise_for_status()
 
-def main():
-    send_sms('Hello World !')
+async def get_session() -> ClientSession:
+    async with ClientSession() as session:
+        return session
+
+
+async def main():
+    # session = await get_session()
+    sms = partial(send_sms, USER, PASSWORD)
+    await sms('Hello World !')
     return 0
  
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())

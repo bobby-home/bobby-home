b48cc44639506878817f96320deb07048ccabef3
Maxime Moreau <contact@maxime-moreau.fr>
2020-03-01 17:55:59 +0100

    wip(motion detection with camera)

--- raspberrypi_central/attachments/app/alarm.py
+++ raspberrypi_central/attachments/app/alarm.py
@@ -1,25 +1,7 @@
-from alarm_requests import AlarmRequests
-import os
-import asyncio
-import logging
+from mqtt.mqtt_camera import MqttCamera
+from motion.camera_manager import CameraManager as CM
+from motion.camera_factory import camera_factory
 
-
-api_key = os.environ['API_KEY']
-api_url = os.environ['API_URL']
-device_id = os.environ['DEVICE_ID']
-
-def loop_exception_handler(loop, context):
-    logging.error(f"Caught exception : {context['exception']}")
-
-def notify_motion():
-    reqs = AlarmRequests(api_key, api_url, device_id)
-
-    loop = asyncio.get_event_loop()
-    loop.set_exception_handler(loop_exception_handler)
-
-    print('run even loop tasks')
-    future = asyncio.ensure_future(reqs.send_all())
-    loop.run_until_complete(future)
-    print('done even loop')
-
-notify_motion()
+print('coucou')
+manager = CM(camera_factory)
+mqtt_camera_manager = MqttCamera(manager)

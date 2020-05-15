c94b5babca4b3a3dc1af2d059d0b3b57356e24be
Maxime Moreau <contact@maxime-moreau.fr>
2020-05-15 09:33:47 +0200

    wip

---
+++ raspberrypi_central/webapp/app/alarm/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from . import models
+
+class AlarmStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AlarmStatus
+        fields = "__all__"
---
+++ raspberrypi_central/webapp/app/alarm/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from api_keys.permissions import HasAPIAccess
+from rest_framework import viewsets, generics, mixins, status
+from rest_framework.response import Response
+from . import serializers
+from . import models
+
+from django.shortcuts import render
+
+
+def index(request):
+    status = models.AlarmStatus.objects.all()
+
+    context = {'status': status}
+    return render(request, 'home.html', context)
+
+class AlarmStatusViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = (HasAPIAccess,)
+
+    queryset = models.AlarmStatus.objects.all()
+    serializer_class = serializers.AlarmStatusSerializer
+
+    def create(self, request):
+        # alarm_status = self.get_object()
+        serializer = serializers.AlarmStatusSerializer(data=request.data)
+        if serializer.is_valid():
+            status = request.data.get('status')
+            if (status == 'on'):
+                models.AlarmStatus.objects.update_or_create(
+                    is_active=True
+                )
+            elif (status == 'off'):
+                models.AlarmStatus.objects.update_or_create(
+                    is_active=False
+                )
+            return Response('WIP!')
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response('bad!')
+
+    def list(self, request):
+        alarm_status = models.AlarmStatus.objects.all()
+        print(alarm_status)
+        return Response('hello')
+

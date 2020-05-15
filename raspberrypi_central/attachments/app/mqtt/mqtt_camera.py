22b196993c63bbfe7f269076caee1f6889a59595
Maxime Moreau <contact@maxime-moreau.fr>
2020-05-15 09:18:58 +0200

    feat(motion detect: on/off): with mqtt topic

---
+++ raspberrypi_central/attachments/app/mqtt/mqtt_camera.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+import os
+import ssl
+from motion.camera_manager import CameraManager
+
+class MqttCamera():
+    def __init__(self, camera_manager: CameraManager):
+        self._camera_manager = camera_manager
+        self._mqtt_client = self._mqtt_connect()
+
+    def _mqtt_on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe(os.environ['MQTT_ALARM_CAMERA_TOPIC'])
+
+    def _mqtt_on_message(self, client, userdata, msg):
+        message = msg.payload.decode()
+
+        if message == 'on':
+            print('waking up the alarm system.')
+            self._camera_manager.running = True
+        elif message == 'off':
+            print('alarm is off')
+            self._camera_manager.running = False
+
+    def _mqtt_connect(self) -> mqtt.Client:
+        print('mqtt connection')
+        client = mqtt.Client()
+        client.username_pw_set(os.environ['MQTT_USER'], os.environ['MQTT_PASSWORD'])
+
+        client.connect(os.environ['MQTT_HOSTNAME'], int(os.environ['MQTT_PORT']), keepalive=60)
+
+        client.on_connect = self._mqtt_on_connect
+        client.on_message = self._mqtt_on_message
+
+        client.loop_forever()
+        return client
+
+# WIP: work with TLS.
+# os.environ['REQUESTS_CA_BUNDLE'] = "/usr/local/share/ca-certificates/ca.cert"
+# os.environ['REQUESTS_CA_BUNDLE'] = os.path.join(
+#     '/etc/ssl/certs/',
+#     'ca-certificates.crt')
+
+# CA = "./ca.crt"
+# CERT_FILE = "./test_client.crt"
+# KEY_FILE = "./test_client.key"
+
+# client.tls_set(ca_certs=CA, certfile=CERT_FILE,
+#                 keyfile=KEY_FILE, tls_version=ssl.PROTOCOL_TLSv1_2)
+# client.tls_insecure_set(True)
+

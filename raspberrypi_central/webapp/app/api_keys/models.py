686e03540ede22869853b8e7a0bd89ac631061f2
Maxime Moreau <contact@maxime-moreau.fr>
2020-06-25 10:56:42 +0200

    clean: move code to the right place

--- raspberrypi_central/webapp/app/api_keys/models.py
+++ raspberrypi_central/webapp/app/api_keys/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .helpers import generate_key
 
 class APIKey(models.Model):
 
@@ -11,3 +12,12 @@ class APIKey(models.Model):
 
     label = models.CharField(max_length=100, unique=True)
     key = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.key
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = generate_key()
+            # messages.add_message(request, messages.WARNING, ('The API Key for %s is %s. Please note it since you will not be able to see it again.' % (self.label, obj.key)))
+        super(APIKey, self).save(*args, **kwargs)

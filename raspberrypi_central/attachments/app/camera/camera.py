59a1a5db5d145ff6b81c6860daf1eeb010d53f89
Maxime Moreau <contact@maxime-moreau.fr>
2020-06-25 11:52:15 +0200

    fix(celery task call)

--- raspberrypi_central/attachments/app/camera/camera.py
+++ raspberrypi_central/attachments/app/camera/camera.py
@@ -12,6 +12,6 @@ class Camera():
 
     def _presenceCallback(self, presence: bool, picture_path: str):
         print(f'presence: {presence}')
-        send_telegram_message.apply_async('Une présence étrangère a été détectée chez vous.', picture_path)
+        send_telegram_message.apply_async(args=['Une présence étrangère a été détectée chez vous.', picture_path])
         # s = Sound()
         # s.alarm()

67262721ee8b26d2a3dc140384de7645f6b08908
Maxime Moreau <contact@maxime-moreau.fr>
2020-06-27 19:04:35 +0200

    clean(devices admin)

--- raspberrypi_central/webapp/app/devices/admin.py
+++ raspberrypi_central/webapp/app/devices/admin.py
@@ -3,11 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-class DeviceAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-
 admin.site.register(models.Location)
 admin.site.register(models.DeviceType)
-admin.site.register(models.Device, DeviceAdmin)
+admin.site.register(models.Device)
 # admin.site.register(SensorInformation)
 # admin.site.register(Sensor)

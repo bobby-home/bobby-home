2ec46560964791adae4ae653db199a0cb378a2d3
Maxime Moreau <contact@maxime-moreau.fr>
2020-07-25 22:10:09 +0200

    feat(mqtt): on motion picture

--- raspberrypi_central/webapp/app/mqtt.py
+++ raspberrypi_central/webapp/app/mqtt.py
@@ -2,6 +2,9 @@ from celery import Celery
 import paho.mqtt.client as mqtt
 import os
 import json
+from pathlib import Path
+import uuid
+
 
 celery_client = Celery('tasks', broker='amqp://admin:mypass@rabbit:5672')
 
@@ -28,10 +31,25 @@ def on_motion_camera(client, userdata, msg):
         'device_id': payload['device_id']
     }
 
-    # @TODO
     celery_client.send_task('security.camera_motion_detected', kwargs=data)
+
+def on_motion_picture(client, userdata, msg):
+    random = uuid.uuid4()
+    file_path = Path(f'./pictures/{random}.jpg').resolve()
+
+    with open(file_path, 'wb+') as file:
+        file.write(msg.payload)
+
+    data = {
+        # 'device_id': payload['device_id'],
+        'file_path': str(file_path)
+    }
+
+    celery_client.send_task('security.camera_motion_picture', kwargs=data)
+
 
 mqtt_client.subscribe('motion/camera', qos=1)
 mqtt_client.message_callback_add('motion/camera', on_motion_camera)
+mqtt_client.message_callback_add('motion/picture', on_motion_picture)
 
 mqtt_client.loop_forever()

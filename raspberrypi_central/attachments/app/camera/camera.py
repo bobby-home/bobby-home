b038c8dc52b8d1887ba813c21e2ddec60be0df53
Maxime Moreau <contact@maxime-moreau.fr>
2020-07-31 11:04:27 +0200

    fix: mqtt publish wasn't working in _presenceCallback
    I have to create a new connection in this class, otherwise it's not working: when we're publishing messages, we don't have errors but the broker doesn't receive anything!

--- raspberrypi_central/attachments/app/camera/camera.py
+++ raspberrypi_central/attachments/app/camera/camera.py
@@ -3,11 +3,28 @@ from pathlib import Path
 from sound.sound import Sound
 import paho.mqtt.client as mqtt
 import json
+import os
 
+
+def create_mqtt_client(mqtt_user: str, mqtt_pswd: str, mqtt_hostname: str, mqtt_port: str):
+    client = mqtt.Client(client_id='rpi4-alarm-motion-DETECT', clean_session=False)
+    client.username_pw_set(mqtt_user, mqtt_pswd)
+
+    client.connect(mqtt_hostname, int(mqtt_port), keepalive=120)
+
+    # mqtt.Client
+    return client
 
 class Camera():
 
-    def __init__(self, mqtt_client):
+    def __init__(self, _):
+        mqtt_client = create_mqtt_client(
+            os.environ['MQTT_USER'],
+            os.environ['MQTT_PASSWORD'],
+            os.environ['MQTT_HOSTNAME'],
+            os.environ['MQTT_PORT']
+        )
+
         self.mqtt_client = mqtt_client
 
     def start(self):
@@ -19,12 +36,12 @@ class Camera():
             'device_id': 'some device id',
         }
 
-        infot = self.mqtt_client.publish('motion/camera', payload=json.dumps(payload), qos=1)
+        self.mqtt_client.publish('motion/camera', payload=json.dumps(payload), qos=1)
 
         with open(picture_path, 'rb') as image:
             filecontent = image.read()
             byteArr = bytes(filecontent)
 
-        self.mqtt_client.publish('motion/picture', payload=byteArr)
+        self.mqtt_client.publish('motion/picture', payload=byteArr, qos=1)
         # s = Sound()
         # s.alarm()

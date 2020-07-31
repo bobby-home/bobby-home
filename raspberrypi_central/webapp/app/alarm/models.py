286565e8383b4d4634fa855ad1181cc62256d676
Maxime Moreau <contact@maxime-moreau.fr>
2020-07-31 21:32:43 +0200

    feat(alarm): create schedule db entries on save to turn on/off

--- raspberrypi_central/webapp/app/alarm/models.py
+++ raspberrypi_central/webapp/app/alarm/models.py
@@ -1,4 +1,6 @@
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
 from django.db import models
+import pytz
 from . import tasks
 
 
@@ -16,6 +18,58 @@ class AlarmSchedule(models.Model):
     friday    = models.BooleanField()
     saturday  = models.BooleanField()
     sunday    = models.BooleanField()
+
+    turn_on_task = models.OneToOneField(PeriodicTask, blank=True, null=True, on_delete=models.CASCADE, related_name='alarm_schedule_on')
+    turn_off_task = models.OneToOneField(PeriodicTask, blank=True, null=True, on_delete=models.CASCADE, related_name='alarm_schedule_off')
+
+
+    def save(self, *args, **kwargs):
+        days = []
+
+        possible_days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+        """
+        Celery "day_of_the_week" inputs: "A (list of) integers from 0-6, where Sunday = 0 and Saturday = 6, that represent the days of a week that execution should occur."
+        https://docs.celeryproject.org/en/latest/reference/celery.schedules.html#celery.schedules.crontab.day_of_week
+        So we are mapping values to int.
+        """
+        for day_int, day_str in enumerate(possible_days, start=0):
+            if getattr(self, day_str):
+                days.append(str(day_int))
+    
+        cron_days = ','.join(days)
+        print(cron_days)
+        europe_tmz = pytz.timezone('Europe/Paris')
+
+        schedule_turn_on_alarm, _ = CrontabSchedule.objects.get_or_create(
+            minute=self.minute_start,
+            hour=self.hour_start,
+            day_of_week=cron_days,
+            # @TODO: define timezone of the HOUSE.
+            timezone=europe_tmz
+        )
+
+        self.turn_on_task = PeriodicTask.objects.update_or_create(
+            name=f'Turn on the alarm for schedule {self.pk}',
+            task='alarm.set_alarm_on',
+            defaults={'crontab': schedule_turn_on_alarm}
+        )
+
+        schedule_turn_off_alarm, _ = CrontabSchedule.objects.get_or_create(
+            minute=self.minute_end,
+            hour=self.hour_end,
+            day_of_week=cron_days,
+            # @TODO: define timezone of the HOUSE.
+            timezone=europe_tmz
+        )
+
+        self.turn_off_task = PeriodicTask.objects.update_or_create(
+            name=f'Turn off the alarm for schedule {self.pk}',
+            crontab=schedule_turn_off_alarm,
+            defaults={'crontab': schedule_turn_on_alarm}
+        )
+
+        super().save(*args, **kwargs)
 
 
 class AlarmStatus(models.Model):

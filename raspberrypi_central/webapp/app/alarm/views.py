c0c1cd047908268461348ce0a6aa2accfe99b321
Maxime Moreau <contact@maxime-moreau.fr>
2020-07-31 14:41:49 +0200

    feat(alarm): view as class instead of function

--- raspberrypi_central/webapp/app/alarm/views.py
+++ raspberrypi_central/webapp/app/alarm/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+
 from api_keys.permissions import HasAPIAccess
 from rest_framework import viewsets, generics, mixins, status
 from rest_framework.response import Response
@@ -6,18 +8,23 @@ from rest_framework import status
 from . import serializers
 from . import models
 from . import tasks
-from . import forms
+from alarm.forms import AlarmScheduleForm
 
-from django.shortcuts import render
+class AlarmView(View):
+    form_class = AlarmScheduleForm
+    template_name = 'alarm/schedule.html'
 
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
-def index(request):
-    status = models.AlarmStatus.objects.all()
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            return HttpResponseRedirect('/success/')
 
-    form = forms.AlarmScheduleForm()
-
-    context = {'status': status, 'form': form}
-    return render(request, 'alarm/schedule.html', context)
+        return render(request, self.template_name, {'form': form})
 
 class AlarmStatusViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     permission_classes = (HasAPIAccess,)

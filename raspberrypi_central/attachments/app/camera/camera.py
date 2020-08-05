362bf1ebfc6fc5fb9260a631890de82cd6625783
Maxime Moreau <contact@maxime-moreau.fr>
2020-08-05 22:04:13 +0200

    feat: tensorflow lite object detection to detect people

--- raspberrypi_central/attachments/app/camera/camera.py
+++ raspberrypi_central/attachments/app/camera/camera.py
@@ -11,8 +11,7 @@ class Camera():
         self.mqtt_client = get_mqtt_client(client_name='rpi4-alarm-motion-DETECT')
 
     def start(self):
-        from picamera import PiCamera
-        DetectMotion(PiCamera(), self._presenceCallback)
+        DetectMotion(self._presenceCallback)
 
     def _presenceCallback(self, presence: bool, picture_path: str):
         payload = {
@@ -22,10 +21,10 @@ class Camera():
 
         self.mqtt_client.publish('motion/camera', payload=json.dumps(payload), qos=1)
 
-        with open(picture_path, 'rb') as image:
-            filecontent = image.read()
-            byteArr = bytes(filecontent)
+        # with open(picture_path, 'rb') as image:
+        #     filecontent = image.read()
+        #     byteArr = bytes(filecontent)
 
-        self.mqtt_client.publish('motion/picture', payload=byteArr, qos=1)
+        # self.mqtt_client.publish('motion/picture', payload=byteArr, qos=1)
         # s = Sound()
         # s.alarm()
--- raspberrypi_central/attachments/app/camera/detect_motion.py
+++ raspberrypi_central/attachments/app/camera/detect_motion.py
@@ -1,160 +1,120 @@
-from picamera.array import PiRGBArray
 from typing import Callable
-import argparse
-import warnings
 import datetime
 import json
 import time
-import cv2
-from camera.tempimage import TempImage
+import re
 
-conf = json.load(open('camera/motion-conf.json', 'r'))
+import io
+import time
 
-def grab_contours(cnts):
-    # if the length the contours tuple returned by cv2.findContours
-    # is '2' then we are using either OpenCV v2.4, v4-beta, or
-    # v4-official
-    if len(cnts) == 2:
-        cnts = cnts[0]
+from fractions import Fraction
 
-    # if the length of the contours tuple is '3' then we are using
-    # either OpenCV v3, v4-pre, or v4-alpha
-    elif len(cnts) == 3:
-        cnts = cnts[1]
+import numpy as np
+import picamera
 
-    # otherwise OpenCV has changed their cv2.findContours return
-    # signature yet again and I have no idea WTH is going on
-    else:
-        raise Exception(("Contours tuple must have length 2 or 3, "
-            "otherwise OpenCV changed their cv2.findContours return "
-            "signature yet again. Refer to OpenCV's documentation "
-            "in that case"))
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
 
-    # return the actual contours array
-    return cnts
-
-
-def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
-
-    # return the resized image
-    return resized
 
 class DetectMotion():
 
-    def __init__(self, pi_camera, presenceCallback: Callable[[bool, str], None]):
-        # Initialize RPI Camera.
-        self.camera = pi_camera
-        self.camera.resolution = (640, 480)
-        self.camera.framerate = 32
-        self.rawCapture = PiRGBArray(self.camera, size=(640, 480))
-        # allow the self.camera to warmup
-        time.sleep(0.1)
-
+    def __init__(self, presenceCallback: Callable[[bool, str], None]):
+        print('Starting detect motion')
         # State
-        self.avg = None
         self.lastUploaded = datetime.datetime.now()
-        self.motionCounter = 0
 
-        self.presenceCallback = presenceCallback
+        self._presenceCallback = presenceCallback
         self._run()
 
-    def _run(self):
-        # capture frames from the camera continuously (so it's a video)
-        for f in self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image
-            frame = f.array
-            text = "Unoccupied"
+    def _load_labels(self, path):
+        """Loads the labels file. Supports files with or without index numbers."""
+        with open(path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            labels = {}
+            for row_number, content in enumerate(lines):
+                pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
 
-            frame = resize(frame, width=500)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-            # if the average frame is None, initialize it
-            if self.avg is None:
-                print("[INFO] starting background model...")
-                self.avg = gray.copy().astype("float")
-                self.rawCapture.truncate(0)
-                continue
-
-            # accumulate the weighted average between the current frame and
-            # previous frames, then compute the difference between the current
-            # frame and running average
-            cv2.accumulateWeighted(gray, self.avg, 0.5)
-            frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(self.avg))
-            
-            # threshold the delta image, dilate the thresholded image to fill
-            # in holes, then find contours on thresholded image
-            thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, None, iterations=2)
-            cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = grab_contours(cnts)
-        
-            # loop over the contours
-            for c in cnts:
-
-                # if the contour is too small, ignore it
-                if cv2.contourArea(c) < conf["min_area"]:
-                    continue
-
-                # compute the bounding box for the contour, draw it on the frame,
-                # and update the text
-                (x, y, w, h) = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                text = "Occupied"
-
-                if text == "Occupied":
-                    timestamp = datetime.datetime.now()
-
-                    # check to see if enough time has passed between uploads
-                    if (timestamp - self.lastUploaded).seconds >= conf["min_upload_seconds"]:
-                        # increment the motion counter
-                        self.motionCounter += 1
-
-                        # check to see if the number of frames with consistent motion is
-                        # high enough
-                        if self.motionCounter >= conf["min_motion_frames"]:
-                            # write the image to temporary file
-                            t = TempImage()
-                            print(f'Write {t.path} image.')
-                            cv2.imwrite(t.path, frame)
-
-                            self.presenceCallback(True, t.path)
-
-                            # update the last uploaded timestamp and reset the motion
-                            # counter
-                            self.lastUploaded = timestamp
-                            self.motionCounter = 0
-
-                # otherwise, the room is not occupied
+                if len(pair) == 2 and pair[0].strip().isdigit():
+                    labels[int(pair[0])] = pair[1].strip()
                 else:
-                    self.motionCounter = 0
-                    self.presenceCallback(False, None)
+                    labels[row_number] = pair[0].strip()
         
-            # clear the stream in preparation for the next frame
-            self.rawCapture.truncate(0)
+        return labels
+
+    def _set_input_tensor(self, interpreter, image):
+        """Sets the input tensor."""
+        tensor_index = interpreter.get_input_details()[0]['index']
+        input_tensor = interpreter.tensor(tensor_index)()[0]
+        input_tensor[:, :] = image
+
+    def _get_output_tensor(self, interpreter, index):
+        """Returns the output tensor at the given index."""
+        output_details = interpreter.get_output_details()[index]
+        tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+        return tensor
+
+    def _detect_objects(self, interpreter, image, threshold):
+        """Returns a list of detection results, each a dictionary of object info."""
+        self._set_input_tensor(interpreter, image)
+        interpreter.invoke()
+
+        # Get all output details
+        boxes = self._get_output_tensor(interpreter, 0)
+        classes = self._get_output_tensor(interpreter, 1)
+        scores = self._get_output_tensor(interpreter, 2)
+        count = int(self._get_output_tensor(interpreter, 3))
+
+        results = []
+        for i in range(count):
+            if scores[i] >= threshold:
+                result = {
+                    'bounding_box': boxes[i],
+                    'class_id': classes[i],
+                    'score': scores[i]
+                }
+            
+            results.append(result)
+
+        return results
+
+
+    def _run(self):
+        CAMERA_WIDTH = 640
+        CAMERA_HEIGHT = 480
+
+        args = {
+            'model': 'tensorflow-object-detection/data/detect.tflite',
+            'labels': 'tensorflow-object-detection/data/coco_labels.txt',
+            'threshold': 0.4
+        }
+
+        print(f'Starting object detection {args}')
+
+        labels = self._load_labels(args['labels'])
+        interpreter = Interpreter(args['model'])
+        interpreter.allocate_tensors()
+        _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
+
+        print('Starting the picamera')
+
+        with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=1) as camera:
+            stream = io.BytesIO()
+
+            for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+                stream.seek(0)
+                image = Image.open(stream).convert('RGB').resize((input_width, input_height), Image.ANTIALIAS)
+
+                results = self._detect_objects(interpreter, image, args['threshold'])
+                print(f'results={results}')
+
+                for obj in results:
+                    label = labels[obj['class_id']]
+                    score = obj['score']
+
+                    print(f'We found {label} with score of {score}')
+                    if person == 'person':
+                        self._presenceCallback(True, None)
+
+                stream.seek(0)
+                stream.truncate()
+

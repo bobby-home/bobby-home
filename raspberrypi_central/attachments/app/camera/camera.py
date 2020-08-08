c853fae4887bbfa422ec47f95dd6548f8fd143a6
Maxime Moreau <contact@maxime-moreau.fr>
2020-08-08 20:01:40 +0200

    fix: mqtt loop_start to run network loop in a thread to avoid disconnect
    Otherwise the main thread is locked by the DetectMotion thing. So, the mqtt_client doesn't send every x seconds the PINGREQ. Fix issue #45

--- raspberrypi_central/attachments/app/camera/camera.py
+++ raspberrypi_central/attachments/app/camera/camera.py
@@ -9,9 +9,11 @@ class Camera():
 
     def __init__(self, get_mqtt_client, device_id):
         self._device_id = device_id
-        self.mqtt_client = get_mqtt_client(client_name=f'{device_id}-rpi4-alarm-motion-DETECT')
+        self.get_mqtt_client = get_mqtt_client
 
     def start(self):
+        self.mqtt_client = self.get_mqtt_client(client_name=f'{self._device_id}-rpi4-alarm-motion-DETECT')
+        self.mqtt_client.loop_start()
         DetectMotion(self._presenceCallback)
 
     def _presenceCallback(self, presence: bool, byteArr):
@@ -22,5 +24,6 @@ class Camera():
         self.mqtt_client.publish('motion/camera', payload=json.dumps(payload), qos=1)
 
         self.mqtt_client.publish('motion/picture', payload=byteArr, qos=1)
+
         # s = Sound()
         # s.alarm()

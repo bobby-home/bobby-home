ff381107de87cf063443b78a4d2f2b57265a3f28
Maxime Moreau <contact@maxime-moreau.fr>
2020-08-09 11:28:15 +0200

    feat: alarm mqtt network loop as thread

--- raspberrypi_central/attachments/app/alarm.py
+++ raspberrypi_central/attachments/app/alarm.py
@@ -10,6 +10,7 @@ from mqtt.mqtt_client import get_mqtt_client
 import os
 
 mqtt_client = get_mqtt_client(f"{os.environ['DEVICE_ID']}-rpi4-alarm-motion")
+mqtt_client.loop_start()
 
 MQTT_ALARM_CAMERA_TOPIC = os.environ['MQTT_ALARM_CAMERA_TOPIC']
 

225a65910add8bf4570cad1f2188e7a58582fe22
Maxime Moreau <contact@maxime-moreau.fr>
2020-08-15 16:09:03 +0200

    feat: task to send sound status to true when motion detected

--- raspberrypi_central/webapp/app/alarm/tasks.py
+++ raspberrypi_central/webapp/app/alarm/tasks.py
@@ -9,6 +9,28 @@ from house import models as house_models
 from devices import models as device_models
 from notification.tasks import send_message
 
+def create_mqtt_client(mqtt_user: str, mqtt_pswd: str, mqtt_hostname: str, mqtt_port: str):
+    client = mqtt.Client(client_id='rpi4-mqtt-listen-django', clean_session=False)
+    client.username_pw_set(mqtt_user, mqtt_pswd)
+
+    client.connect(mqtt_hostname, int(mqtt_port), keepalive=120)
+
+    return client
+
+def create_mqtt_client(mqtt_user: str, mqtt_pswd: str, mqtt_hostname: str, mqtt_port: str, client_name = None):
+
+    if client_name is None:
+        clean_session = True
+    else:
+        False
+
+    client = mqtt.Client(client_id=client_name, clean_session=client_name)
+    client.username_pw_set(mqtt_user, mqtt_pswd)
+
+    client.connect(mqtt_hostname, int(mqtt_port), keepalive=120)
+
+    return client
+
 
 class AlarmMessaging():
 
@@ -44,6 +66,19 @@ def camera_motion_picture(self, picture_path):
     send_message.apply_async(kwargs=kwargs)
 
 
+@shared_task(name="security.play_sound")
+def play_sound(motion_came_from_device_id: str):
+    # device = device_models.Device.objects.get(device_id=device_id)
+    mqtt_client = create_mqtt_client(
+        os.environ['MQTT_USER'],
+        os.environ['MQTT_PASSWORD'],
+        os.environ['MQTT_HOSTNAME'],
+        os.environ['MQTT_PORT']
+    )
+
+    mqtt_client.publish('status/sound', str(True), qos=1)
+
+
 @shared_task(name="security.camera_motion_detected")
 def camera_motion_detected(device_id: str):
     device = device_models.Device.objects.get(device_id=device_id)
@@ -54,7 +89,9 @@ def camera_motion_detected(device_id: str):
     kwargs = {
         'message': f'Une présence étrangère a été détectée chez vous depuis {device_id} {location.structure} {location.sub_structure}'
     }
+
     send_message.apply_async(kwargs=kwargs)
+    play_sound.apply_async(kwargs={'motion_came_from_device_id': device_id})
 
 
 @shared_task(name="alarm.set_alarm_off")

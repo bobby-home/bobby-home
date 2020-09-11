fd99ece7db2b8a84e2e565aadb72e864c57fa6eb
Maxime Moreau <contact@maxime-moreau.fr>
2020-09-11 08:42:37 +0300

    improve: videostream lazy load webcam with opencv

--- raspberrypi_central/attachments/app/camera/videostream.py
+++ raspberrypi_central/attachments/app/camera/videostream.py
@@ -1,6 +1,3 @@
-from camera.webcamvideostream import WebcamVideoStream
-
-
 class VideoStream:
 	def __init__(self, processFrame, resolution, framerate, src=0, usePiCamera=False):
 
@@ -10,4 +7,5 @@ class VideoStream:
 			from camera.pivideostream import PiVideoStream
 			self.stream = PiVideoStream(processFrame, resolution, framerate)
 		else:
+            from camera.webcamvideostream import WebcamVideoStream
 			self.stream = WebcamVideoStream(processFrame, resolution, framerate, src)

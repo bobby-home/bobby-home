cc072fcab7244dc9e46358f770398b82fd9d7379
Maxime Moreau <contact@maxime-moreau.fr>
2020-09-19 00:17:12 +0300

    feat: messaging send status as real boolean

--- raspberrypi_central/webapp/app/alarm/messaging.py
+++ raspberrypi_central/webapp/app/alarm/messaging.py
@@ -1,9 +1,14 @@
+import struct
+
+
 class MqttStatus():
     def __init__(self, mqtt_client):
         self._mqtt_client = mqtt_client
 
-    def publish(self, topic, message):
-        self._mqtt_client.publish(topic, message, qos=1, retain=True)
+    def publish(self, topic, message: bool):
+        status_bytes = struct.pack('?', message)
+        print(f'send status: {status_bytes} on {topic}')
+        self._mqtt_client.publish(topic, status_bytes, qos=1, retain=True)
 
 
 class SpeakerMessaging():

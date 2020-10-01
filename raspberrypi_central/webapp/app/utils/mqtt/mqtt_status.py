c4784ccf9354bbe837ea153b6af45082deb11c36
Maxime Moreau <contact@maxime-moreau.fr>
2020-10-01 12:36:49 +0300

    fix: mqtt status without data

--- raspberrypi_central/webapp/app/utils/mqtt/mqtt_status.py
+++ raspberrypi_central/webapp/app/utils/mqtt/mqtt_status.py
@@ -2,7 +2,6 @@ import struct
 import json
 from decimal import Decimal
 
-
 from utils.mqtt import MQTT
 
 
@@ -10,7 +9,7 @@ class MqttBooleanStatus:
     def __init__(self, mqtt_client: MQTT):
         self._mqtt_client = mqtt_client
 
-    def publish(self, topic, message: bool):
+    def publish(self, topic, message: bool) -> None:
         status_bytes = struct.pack('?', message)
         self._mqtt_client.publish(topic, status_bytes, qos=1, retain=True)
 
@@ -25,8 +24,12 @@ class MqttJsonStatus:
     def __init__(self, mqtt_client: MQTT):
         self._mqtt_client = mqtt_client
 
-    def publish(self, topic, msg: bool, data):
+    def publish(self, topic, msg: bool, data=None) -> None:
+        if data is None:
+            data = {}
+
         data['status'] = msg
+
         encoded_data = json.dumps(data, default=decimal_default)
 
         print(encoded_data)

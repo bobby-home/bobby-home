093613a29ab7fd77cb5d18c7cad0283cbc0095b8
Maxime Moreau <contact@maxime-moreau.fr>
2020-10-25 21:54:58 +0200

    fix: jinja2 django static helper

--- raspberrypi_central/webapp/app/hello_django/jinja2.py
+++ raspberrypi_central/webapp/app/hello_django/jinja2.py
@@ -1,13 +1,13 @@
 # from: https://docs.djangoproject.com/en/3.0/topics/templates/#module-django.template.backends.django
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from jinja2 import Environment
+from django.templatetags.static import static
 
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage.url,
+        'static': static,
         'url': reverse,
     })
     return env

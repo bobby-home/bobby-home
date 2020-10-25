3072bc4e09f3ac899907d7f912bed3248fca7fae
Maxime Moreau <contact@maxime-moreau.fr>
2020-10-25 21:55:48 +0200

    feat: serve media during dev

--- raspberrypi_central/webapp/app/hello_django/urls.py
+++ raspberrypi_central/webapp/app/hello_django/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
 
@@ -11,10 +12,11 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path('__debug__/', include(debug_toolbar.urls)),
-#     ] + urlpatterns
-#     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # https://docs.djangoproject.com/en/3.0/howto/static-files/#serving-files-uploaded-by-a-user-during-development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

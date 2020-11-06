2df80dd39fda008bfd9d554abcd1c6bf8c2a1b3a
Maxime Moreau <contact@maxime-moreau.fr>
2020-11-06 22:28:39 +0200

    feat(alarm): camera roi & links

--- raspberrypi_central/webapp/app/alarm/models.py
+++ raspberrypi_central/webapp/app/alarm/models.py
@@ -27,6 +27,17 @@ class AlarmStatus(models.Model):
         return f'Status is {self.running} for {self.device}'
 
 
+class CameraROI(models.Model):
+    device = models.OneToOneField(Device, on_delete=models.PROTECT)
+
+    """
+    We save the picture where the user defined the ROI.
+    This could be useful to check if the camera moves to let know the user
+    because the ROI could become wrong.
+    """
+    define_picture = models.ImageField()
+
+
 class CameraRectangleROI(models.Model):
     # ? We might rework the max_digits & decimal_places here
     # I didn't really know what to put here. Seems good but may not be optimized.
@@ -36,10 +47,7 @@ class CameraRectangleROI(models.Model):
     w = models.DecimalField(max_digits=8, decimal_places=4)
     h = models.DecimalField(max_digits=8, decimal_places=4)
 
-    definition_width = models.DecimalField(max_digits=8, decimal_places=4)
-    definition_height = models.DecimalField(max_digits=8, decimal_places=4)
-
-    device = models.ForeignKey(Device, on_delete=models.PROTECT)
+    camera_roi = models.ForeignKey(CameraROI, on_delete=models.CASCADE)
 
     # TODO: use a service to change rectangle ROI.
     # def save(self, *args, **kwargs):

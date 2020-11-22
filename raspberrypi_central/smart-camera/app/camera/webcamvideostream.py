56b3a521c5e71837346450dab8200e3fa040feb1
Maxime Moreau <contact@maxime-moreau.fr>
2020-11-22 20:12:30 +0200

    improve(opencv cam): check if open + frame not none

--- raspberrypi_central/smart-camera/app/camera/webcamvideostream.py
+++ raspberrypi_central/smart-camera/app/camera/webcamvideostream.py
@@ -6,6 +6,11 @@ class WebcamVideoStream:
         self.processFrame = processFrame
 
         self.stream = cv2.VideoCapture(src)
+
+        # https://stackoverflow.com/questions/48049886/how-to-correctly-check-if-a-camera-is-available
+        if self.stream is None or not self.stream.isOpened():
+            raise Exception(f'Unable to open video source {src} with OpenCV.')
+
         self.stream.set(cv2.CAP_PROP_FPS, framerate)
 
         (width, height) = resolution
@@ -16,10 +21,18 @@ class WebcamVideoStream:
         _ = self.stream.read()
         self._update()
 
+    def __del__(self):
+        self.stream.release()
+
     def _update(self):
         # keep looping infinitely until the thread is stopped
         while True:
             (_, frame) = self.stream.read()
+
+            # Some frames could be None at the beginning.
+            if frame is None:
+                continue
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             self.processFrame(frame)
             # TODO issue #79: self.stream.release() to release resources when turning off the camera.

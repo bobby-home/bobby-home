4f6d4021f213eda1a043aed3d16296a363a252f7
Maxime Moreau <contact@maxime-moreau.fr>
2021-01-19 14:31:46 +0100

    feat(mqtt): check for multiple status
    If the last registered status is `status` then the function returns True.

--- raspberrypi_central/webapp/app/mqtt_services/business/mqtt_services.py
+++ raspberrypi_central/webapp/app/mqtt_services/business/mqtt_services.py
@@ -1,27 +1,35 @@
 from django.utils import timezone
-
 from mqtt_services.models import MqttServicesConnectionStatusLogs
+import os
 
 
 def is_in_status_since(device_id: str, service_name: str, status: bool, since_time) -> bool:
+    """
+        Check if the service_name is asked status since the time asked.
+        The service_name has to have at least one asked status,
+        AND no opposite asked status.
+    """
     now = timezone.now()
 
-    ok_logs = MqttServicesConnectionStatusLogs.objects.filter(
+    logs = MqttServicesConnectionStatusLogs.objects.filter(
         device_id=device_id,
         service_name=service_name,
-        status=status,
-        created_at__range=(since_time, now),)
+        created_at__range=(since_time, now),).order_by('created_at')
 
-    ko_logs = MqttServicesConnectionStatusLogs.objects.filter(
-        device_id=device_id,
-        service_name=service_name,
-        status=not status,
-        created_at__range=(since_time, now),)
+    last_false_result = None
+    last_true_result = None
 
-    if ko_logs.exists():
+    for log in logs:
+        if log.status is not status:
+            last_false_result = log.created_at
+
+        if log.status is status:
+            last_true_result = log.created_at
+
+    if last_true_result is None:
         return False
 
-    if ok_logs.exists():
+    if last_false_result is None:
         return True
 
-    return False
+    return last_false_result < last_true_result
--- raspberrypi_central/webapp/app/mqtt_services/tests.py
+++ raspberrypi_central/webapp/app/mqtt_services/tests.py
@@ -1,6 +1,5 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django.utils import timezone
-
 from alarm.models import AlarmStatus
 from devices.factories import DeviceFactory
 from alarm.business.alarm import is_status_exists
@@ -8,11 +7,11 @@ from mqtt_services.models import MqttServicesConnectionStatusLogs
 from mqtt_services.business.mqtt_services import is_in_status_since
 
 
-class HealthTestCase(TestCase):
+class HealthTestCase(TransactionTestCase):
     def setUp(self) -> None:
         self.device = DeviceFactory()
         self.service_name = 'camera'
-        pass
+        self.now = timezone.now()
 
     def test_is_in_status_since(self):
         now = timezone.now()
@@ -28,20 +27,45 @@ class HealthTestCase(TestCase):
             is_in_status_since(self.device.device_id, self.service_name, False, now)
         )
 
-        """
-        If the service turns on but turn off before the check, it should be false.
-        """
         MqttServicesConnectionStatusLogs.objects.create(
             device_id=self.device.device_id, service_name=self.service_name, status=False,
         )
+
         self.assertFalse(
-            is_in_status_since(self.device.device_id, self.service_name, True, now)
+            is_in_status_since(self.device.device_id, self.service_name, True, now),
+            'If the service turns on but turn off before the check, it should be false.'
+        )
+
+    def test_is_in_status_since_false_true(self):
+        MqttServicesConnectionStatusLogs.objects.create(
+            device_id=self.device.device_id, service_name=self.service_name, status=False,
+        )
+        MqttServicesConnectionStatusLogs.objects.create(
+            device_id=self.device.device_id, service_name=self.service_name, status=True,
+        )
+
+        self.assertTrue(
+            is_in_status_since(self.device.device_id, self.service_name, True, self.now)
+        )
+
+    def test_is_in_status_since_true_false(self):
+        MqttServicesConnectionStatusLogs.objects.create(
+            device_id=self.device.device_id, service_name=self.service_name, status=True,
+        )
+        MqttServicesConnectionStatusLogs.objects.create(
+            device_id=self.device.device_id, service_name=self.service_name, status=False,
+        )
+
+        self.assertFalse(
+            is_in_status_since(self.device.device_id, self.service_name, True, self.now)
         )
 
     def test_is_status_exists(self):
         other_device = DeviceFactory()
 
         AlarmStatus.objects.create(device=self.device, running=True)
+
+
         self.assertTrue(
             is_status_exists(AlarmStatus, device_id=self.device.device_id, running=True)
         )

387b20be55c3dc8c57d6845341fa2f63d101ca0a
Maxime Moreau <contact@maxime-moreau.fr>
2021-01-31 17:42:32 +0100

    docker(core): compose for prod

--- raspberrypi_central/install.sh
+++ raspberrypi_central/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
-sudo apt install -y git vim libffi-dev libssl-dev python python-pip python3-pip pipenv
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git vim libffi-dev libssl-dev python python-pip python3-pip
 
 # Docker
 sudo apt-get remove python-configparser
 sudo apt-get -y install python3-pip
 sudo pip3 install docker-compose
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+
+# enable the pi camera.
+sudo raspi-config nonint do_camera 0

ae4cabf4f4b7040c3382bc9de0491d1ff97138ea
mxmaxime <contact@maxime-moreau.fr>
2021-03-25 20:12:09 +0100

    feat: setup create superuser admin

--- raspberrypi_central/core/app/account/views.py
+++ raspberrypi_central/core/app/account/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
@@ -17,3 +18,19 @@ class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     # success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def clean(self):
+        self.cleaned_data['is_staff'] = True
+        self.cleaned_data['is_superuser'] = True
+        self.cleaned_data['is_admin'] = True
+
+    def form_valid(self, form):
+        form.cleaned_data['password'] = form.cleaned_data['password1']
+
+        # used for data validation, not saved in db.
+        form.cleaned_data.pop('password1', None)
+        form.cleaned_data.pop('password2', None)
+
+        self.object = Account.objects.create_superuser(**form.cleaned_data)
+
+        return HttpResponseRedirect(self.get_success_url())

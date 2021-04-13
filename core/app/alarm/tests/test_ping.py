f4562ea2be5c8eab46616afa5b456ace7bc3d85e
mxmaxime <contact@maxime-moreau.fr>
2021-04-13 18:33:47 +0200

    fix(core): reset ping when it comes back #182

---
+++ core/app/alarm/tests/test_ping.py
@@ -0,0 +1,30 @@
+from alarm.models import Ping
+from alarm.business.alarm import register_ping
+from django.test import TestCase
+
+
+class RegisterTestPing(TestCase):
+    def setUp(self) -> None:
+        self._device_id = 'id5'
+        self._service_name = 'service_name'
+
+    def test_register_ping_create(self):
+        register_ping(self._device_id, self._service_name)
+        pings = Ping.objects.filter(device_id=self._device_id)
+        self.assertEqual(1, len(pings))
+
+    def test_register_update(self):
+        Ping.objects.create(
+                device_id=self._device_id,
+                service_name=self._service_name,
+                consecutive_failures=2,
+        )
+        
+        register_ping(self._device_id, self._service_name)
+        pings = Ping.objects.filter(device_id=self._device_id)
+
+        self.assertEqual(1, len(pings), 'it should update ping, not create a new one')
+        ping = pings[0]
+        self.assertEqual(0, ping.consecutive_failures, 'it should reset consecutive_failures when new ping is registered')
+        self.assertEqual(2, ping.failures, 'it should add consecutive_failures to failures')
+

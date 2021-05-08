fe990e0fa29b06fc9eb0534f0d7c2aec767e3ede
mxmaxime <contact@maxime-moreau.fr>
2021-05-09 01:00:24 +0200

    tests(core): setup main device location

--- core/app/setup/tests.py
+++ core/app/setup/tests.py
@@ -1,3 +1,6 @@
+from devices.models import Device
+from house.models import Location
+from devices.factories import DeviceFactory
 from http import HTTPStatus
 from notification.models import UserSetting, UserTelegramBotChatId
 from account.factories import AccountFactory, DEFAULT_PASSWORD
@@ -31,3 +34,29 @@ class TelegramBotStartViewTestCase(TestCase):
         user_setting = user_settings[0]
         self.assertEqual(user_setting.telegram_chat, chat_id)
         self.assertEqual(user_setting.user, self.user)
+
+
+class MainDeviceLocationViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+        self.device = DeviceFactory(is_main=True, location=None) 
+        self.user = AccountFactory()
+        return super().setUp()
+
+    def test_main_device_location(self):
+        self.client.force_login(self.user)
+
+        data = {
+            'structure': 'house',
+            'sub_structure': 'living_room'
+        }
+
+        response = self.client.post('/setup/device_location', data=data, follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        locations = Location.objects.all()
+        self.assertEqual(len(locations), 1)
+        location = locations[0]
+
+        device = Device.objects.get(pk=self.device.pk)
+        self.assertEqual(location, device.location)

20c486b0f5dca0991fc1c050f8e49f91803cc186
mxmaxime <contact@maxime-moreau.fr>
2021-05-09 02:14:43 +0200

    wip refactor(core): remove schedule business from model to use case

--- core/app/alarm/forms.py
+++ core/app/alarm/forms.py
@@ -1,3 +1,4 @@
+from alarm.use_cases.alarm_schedule import create_alarm_schedule, update_alarm_schedule
 from django import forms
 from django.db import transaction
 from alarm.use_cases.alarm_status import AlarmChangeStatus
@@ -5,10 +6,25 @@ from alarm.models import AlarmSchedule, AlarmStatus
 
 
 class AlarmScheduleForm(forms.ModelForm):
+    _creating: bool
 
     class Meta:
         model = AlarmSchedule
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        self._creating = kwargs.get('instance') is None
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit: bool = True) -> AlarmSchedule:
+        instance = super().save(commit=False)
+
+        if self._creating:
+            create_alarm_schedule(instance)
+        else:
+            update_alarm_schedule(instance)
+
+        return instance
 
 
 class AlarmStatusForm(forms.ModelForm):
--- core/app/alarm/tests/test_alarm_schedule.py
+++ core/app/alarm/tests/test_alarm_schedule.py
@@ -1,14 +1,76 @@
+from alarm.use_cases.alarm_schedule import create_alarm_schedule, update_alarm_schedule
+from alarm.use_cases.alarm_status import alarm_statuses_changed
 from freezegun import freeze_time
 from django.test import TestCase
 from django.utils import timezone
 
 from alarm.business.alarm_schedule import get_current_day
-from alarm.factories import AlarmScheduleFactory
+from alarm.factories import AlarmScheduleFactory, AlarmStatusFactory
 from alarm.models import AlarmSchedule
 from house.factories import HouseFactory
 from datetime import time
-from django_celery_beat.models import PeriodicTask
+from django_celery_beat.models import CrontabSchedule, PeriodicTask
 
+
+class UseCaseAlarmScheduleTestCase(TestCase):
+    def setUp(self) -> None:
+        self.alarm_statuses = [
+            AlarmStatusFactory(),
+            AlarmStatusFactory()
+        ]
+
+        return super().setUp()
+
+    def _check_underlying_objects(self):
+        schedules = AlarmSchedule.objects.all()
+        self.assertEqual(len(schedules), 1)
+        schedule = schedules[0]
+
+        periodic_tasks = PeriodicTask.objects.all()
+        self.assertEqual(len(periodic_tasks), 2)
+
+        turn_on_task = PeriodicTask.objects.get(task='alarm.set_alarm_on')
+        turn_off_task = PeriodicTask.objects.get(task='alarm.set_alarm_off')
+
+        self.assertEqual(schedule.turn_on_task, turn_on_task)
+        self.assertEqual(schedule.turn_off_task, turn_off_task)
+
+        cons = CrontabSchedule.objects.all()
+        self.assertEqual(len(cons), 2)
+
+        turn_on_cron = CrontabSchedule.objects.get(minute='00', hour='06')
+        turn_off_cron = CrontabSchedule.objects.get(minute='00', hour='08')
+
+        self.assertEqual(turn_off_task.crontab, turn_off_cron)
+        self.assertEqual(turn_on_task.crontab, turn_on_cron)
+
+    def test_create_alarm_schedule(self):
+        schedule = AlarmSchedule(
+            start_time='06:00',
+            end_time='08:00',
+            monday=True,
+            tuesday=False,
+            wednesday=True,
+            thursday=False,
+            friday=True,
+            saturday=False
+            sunday=True,
+            alarm_statuses=self.alarm_statuses
+        )
+
+        create_alarm_schedule(schedule)
+        self._check_underlying_objects()
+
+    def test_update_alarm_schedule(self):
+        schedule = AlarmScheduleFactory()
+        
+        schedule.start_time = '04:15'
+        schedule.end_time = '09:30'
+
+        update_alarm_schedule(schedule)
+        
+        # wip: I'm here, I'll continue tomorrow.
+        self._check_underlying_objects()
 
 class AlarmScheduleTestCase(TestCase):
     def setUp(self) -> None:
